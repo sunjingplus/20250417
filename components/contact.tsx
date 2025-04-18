f7a4a935dbb7421ebb88f6b5a6c049d0d7d6c839
sunjing <2914004831@qq.com>
2025-04-18 15:13:18 +0800

    fix

--- components/contact.tsx
+++ components/contact.tsx
@@ -18,7 +18,7 @@ export default function Contact() {
             {t("content")}
           </p>
         </div>
-        {/* <Dropzone /> */}
+        <Dropzone />
       </div>
     </section>
   );
